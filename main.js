069cb00931ef7b8c42d8c2638ea02854aaacc5e5
Shrutidholakia <dholakiashruti@gmail.com>
2017-10-04 17:52:07 -0500

    added main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("This file is linked!");
+var fullName = "Mayor Leodore" ;
+var dept = "Discipline";
+var rate1 = 3.4;
+var rate2 = 5.0;
+var rate3 = 4.2;
+numrate = 3
+avgrate = ( rate1 + rate2 + rate3)/ numrate;
+console.log("Teacher:" + fullName);
+console.log("department: " + dept);
+console.log("Rating:" + rate1 + "," + rate2 +  "," + rate3 );
+console.log("Avg Rating:" + avgrate);
